8644cea5230fa16fba9b768d3ba99e1287e63797
unknown <bgocumlu@users.noreply.github.com>
2025-05-22 01:54:44 +0300

    gh pages fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -67,7 +66,7 @@ const App = () => {
           <Toaster />
           <Sonner />
           <DocumentConfig />
-          <BrowserRouter>
+          <BrowserRouter basename="/shopping-list-pwa/">
             <Routes>
               <Route path="/" element={<Index />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
